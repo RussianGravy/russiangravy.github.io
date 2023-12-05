de121a699dca027010a7b68275aecc555694c03d
RussianGravy <valentinejones1215@gmail.com>
2023-12-05 14:16:26 -0800

    big changes

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
-const maxWidth = screen.width;
-console.log(maxWidth);
+// const maxWidth = screen.width;
+// console.log(maxWidth);
 
-var pgrBubble = new SiteObject("programmer");
-    pgrBubble.setSize((260)*window.innerWidth/maxWidth, 260*window.innerWidth/maxWidth);
-    pgrBubble.setPosition(window.innerWidth*0.5-0.5*pgrBubble.width, window.innerHeight*0.5-0.5*pgrBubble.height);
-    pgrBubble.setVelocity(-8*window.innerWidth/maxWidth, 0);
-    pgrBubble.setAcceleration();
-var illBubble = new SiteObject("illustrator");
-    illBubble.setSize(260*window.innerWidth/maxWidth, 260*window.innerWidth/maxWidth);
-    illBubble.setPosition(window.innerWidth*0.5-0.5*illBubble.width, window.innerHeight*0.5-0.5*illBubble.height);
-    illBubble.setVelocity(8*window.innerWidth/maxWidth, 0);
-    illBubble.setAcceleration();
-var nameBubble = new SiteObject("title");
-    nameBubble.setSize(335*window.innerWidth/maxWidth, 335*window.innerWidth/maxWidth);
-    nameBubble.setPosition(window.innerWidth*0.5-0.5*nameBubble.width, window.innerHeight*0.5-0.5*nameBubble.height);
+// var pgrBubble = new SiteObject("programmer");
+//     pgrBubble.setSize((260)*window.innerWidth/maxWidth, 260*window.innerWidth/maxWidth);
+//     pgrBubble.setPosition(window.innerWidth*0.5-0.5*pgrBubble.width, window.innerHeight*0.5-0.5*pgrBubble.height);
+//     pgrBubble.setVelocity(-8*window.innerWidth/maxWidth, 0);
+//     pgrBubble.setAcceleration();
+// var illBubble = new SiteObject("illustrator");
+//     illBubble.setSize(260*window.innerWidth/maxWidth, 260*window.innerWidth/maxWidth);
+//     illBubble.setPosition(window.innerWidth*0.5-0.5*illBubble.width, window.innerHeight*0.5-0.5*illBubble.height);
+//     illBubble.setVelocity(8*window.innerWidth/maxWidth, 0);
+//     illBubble.setAcceleration();
+// var nameBubble = new SiteObject("title");
+//     nameBubble.setSize(335*window.innerWidth/maxWidth, 335*window.innerWidth/maxWidth);
+//     nameBubble.setPosition(window.innerWidth*0.5-0.5*nameBubble.width, window.innerHeight*0.5-0.5*nameBubble.height);
 
-    var logo = new SiteObject("logo");
-        logo.element.onclick = () => {window.open("otherpages/bio.html");}
-    var penButton = new SiteObject("pen");
-        penButton.element.onclick = () => {window.open("otherpages/illustrator.html");}
-    var compButton = new SiteObject("comp");
-        compButton.element.onclick = () => {window.open("otherpages/programmer.html");}
+//     var logo = new SiteObject("logo");
+//         logo.element.onclick = () => {window.open("otherpages/bio.html");}
+//     var penButton = new SiteObject("pen");
+//         penButton.element.onclick = () => {window.open("otherpages/illustrator.html");}
+//     var compButton = new SiteObject("comp");
+//         compButton.element.onclick = () => {window.open("otherpages/programmer.html");}
     
-    function updateImages(){
-        logo.setSize(nameBubble.width, nameBubble.height);
-        penButton.setSize(illBubble.width, illBubble.height);
-        compButton.setSize(pgrBubble.width, pgrBubble.height);
-    }
-    updateImages();
+//     function updateImages(){
+//         logo.setSize(nameBubble.width, nameBubble.height);
+//         penButton.setSize(illBubble.width, illBubble.height);
+//         compButton.setSize(pgrBubble.width, pgrBubble.height);
+//     }
+//     updateImages();
 
-    window.onresize = () => {
-        console.log("hi");
-        updateImages();
-    }
+//     window.onresize = () => {
+//         console.log("hi");
+//         updateImages();
+//     }
 
 var gameFrame = 0;
 function animate() {
     gameFrame++;
     if(gameFrame%2 == 0){
-        nameBubble.update();
-        pgrBubble.update();
-        illBubble.update();
+        // nameBubble.update();
+        // pgrBubble.update();
+        // illBubble.update();
         updateBackground();
     }
     requestAnimationFrame(animate);
@@ -73,11 +73,11 @@ function updateBackground(){
     // ctx.height = window.innerHeight;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //background
-    ctx.drawImage(temp, 0, tempY, canvas.width, canvas.height);
-    ctx.drawImage(temp, 0, tempY+canvas.height, canvas.width, canvas.height);
+        ctx.drawImage(temp, 0, tempY, canvas.width, canvas.height);
+        ctx.drawImage(temp, 0, tempY+canvas.height, canvas.width, canvas.height);
     //foreground
-    ctx.drawImage(background, bgX, bgY, canvas.width, canvas.height);
-    ctx.drawImage(background, bgX, bgY-canvas.height, canvas.width, canvas.height);
+        ctx.drawImage(background, bgX, bgY, canvas.width, canvas.height);
+        ctx.drawImage(background, bgX, bgY-canvas.height, canvas.width, canvas.height);
     bgY += 0.8;
     tempY -= 0.8;
     if(tempY <= -1*canvas.height){tempY = 0;}
