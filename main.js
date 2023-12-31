6930092d6371f10478ed9397db04c11ca5d898f9
RussianGravy <valentinejones1215@gmail.com>
2023-12-30 18:37:28 -0800

    fixed background code

--- main.js
+++ main.js
@@ -1,46 +1,7 @@
-// const maxWidth = screen.width;
-// console.log(maxWidth);
-
-// var pgrBubble = new SiteObject("programmer");
-//     pgrBubble.setSize((260)*window.innerWidth/maxWidth, 260*window.innerWidth/maxWidth);
-//     pgrBubble.setPosition(window.innerWidth*0.5-0.5*pgrBubble.width, window.innerHeight*0.5-0.5*pgrBubble.height);
-//     pgrBubble.setVelocity(-8*window.innerWidth/maxWidth, 0);
-//     pgrBubble.setAcceleration();
-// var illBubble = new SiteObject("illustrator");
-//     illBubble.setSize(260*window.innerWidth/maxWidth, 260*window.innerWidth/maxWidth);
-//     illBubble.setPosition(window.innerWidth*0.5-0.5*illBubble.width, window.innerHeight*0.5-0.5*illBubble.height);
-//     illBubble.setVelocity(8*window.innerWidth/maxWidth, 0);
-//     illBubble.setAcceleration();
-// var nameBubble = new SiteObject("title");
-//     nameBubble.setSize(335*window.innerWidth/maxWidth, 335*window.innerWidth/maxWidth);
-//     nameBubble.setPosition(window.innerWidth*0.5-0.5*nameBubble.width, window.innerHeight*0.5-0.5*nameBubble.height);
-
-//     var logo = new SiteObject("logo");
-//         logo.element.onclick = () => {window.open("otherpages/bio.html");}
-//     var penButton = new SiteObject("pen");
-//         penButton.element.onclick = () => {window.open("otherpages/illustrator.html");}
-//     var compButton = new SiteObject("comp");
-//         compButton.element.onclick = () => {window.open("otherpages/programmer.html");}
-    
-//     function updateImages(){
-//         logo.setSize(nameBubble.width, nameBubble.height);
-//         penButton.setSize(illBubble.width, illBubble.height);
-//         compButton.setSize(pgrBubble.width, pgrBubble.height);
-//     }
-//     updateImages();
-
-//     window.onresize = () => {
-//         console.log("hi");
-//         updateImages();
-//     }
-
 var gameFrame = 0;
 function animate() {
     gameFrame++;
     if(gameFrame%2 == 0){
-        // nameBubble.update();
-        // pgrBubble.update();
-        // illBubble.update();
         updateBackground();
     }
     requestAnimationFrame(animate);
@@ -49,56 +10,63 @@ animate();
 
 
 //Messy background code below * * * * * * * * * * * * * * * * * |
-var canvas = document.getElementById("background")
-    var ctx = canvas.getContext("2d");
+var aspectratio = 2560/1600;
 
-var background = new Image();
-    background.src = "images/background.png";
-    var bgY = 0;
-
-var foreground = new Image();
-    foreground.src = "images/foreground.png";
-    var fgY = 0;
-
-var scalar = canvas.style.getPropertyValue("width");
-    canvas.addEventListener('resize', () => {
-        scalar = canvas.style.getPropertyValue("width");
-    });
-
-//function updateBackground() {}
-
-var canvas = document.getElementById("background");
+const canvas = document.getElementById("background");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
- var ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
     ctx.width = window.innerWidth;
     ctx.height = window.innerHeight;
-    
-var background = new Image();
-    background.src = "images/foreground.png";
-    var bgY = 0;
 
-var temp = new Image();
-    temp.src = "images/background.png";
-    var tempY = 0;
+const bg = new Image();
+    bg.src = "images/background.png";
+    bg.width = canvas.width;
+    bg.height = bg.width/aspectratio;
 
-function updateBackground(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //background
-        ctx.drawImage(temp, 0, tempY,  window.innerWidth, window.innerHeight);
-        ctx.drawImage(temp, 0, tempY+window.innerHeight, window.innerWidth, window.innerHeight);
-    //foreground
-        ctx.drawImage(background, 0, bgY, window.innerWidth, window.innerHeight);
-        ctx.drawImage(background, 0, bgY-canvas.height, window.innerWidth, window.innerHeight);
-    bgY += 0.8;
-    tempY -= 0.8;
-    if(tempY <= -1*canvas.height){tempY = 0;}
-    if(bgY >= canvas.height){bgY = 0;}
-    if(opacity < 0.98){
-        opacity += 0.015;
-        canvas.style.opacity = "" + opacity;
+const fg = new Image();
+    fg.src = "images/foreground.png";
+    fg.width = canvas.width;
+    fg.height = fg.width/aspectratio;
+
+var drawBackground = (y) => {
+    for(var i = 0; i  < Math.floor(canvas.height/bg.height)+2; i++){
+        ctx.drawImage(bg, 0, y-bg.height*i, bg.width, bg.height);
     }
 }
 
-var opacity = 0;
+var drawForeground = (y) => {
+    for(var i = 0; i  < Math.floor(canvas.height/fg.height)+2; i++){
+        ctx.drawImage(fg, 0, y+fg.height*i, fg.width, fg.height);
+    }
+}
 
+window.onresize = () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.width = window.innerWidth;
+    ctx.height = window.innerHeight;
+    bg.width = window.innerWidth;
+    bg.height = bg.width/aspectratio;
+    fg.width = window.innerWidth;
+    fg.height = fg.width/aspectratio;
+}
+
+const speed = 0.5;
+var cur_y = 0
+function updateBackground(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBackground(-cur_y+(canvas.height-bg.height));
+    drawForeground(cur_y);
+    cur_y -= speed;
+    if(Math.abs(cur_y) >= canvas.height){
+        cur_y = 0;
+    }
+}
+
+// drawImage(image, dx, dy)
+// drawImage(image, dx, dy, dWidth, dHeight)
+// drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+
+// putImageData(imageData, dx, dy)
+// putImageData(imageData, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight)
