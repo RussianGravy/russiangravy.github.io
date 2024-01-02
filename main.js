9afc38143e579b9a088082db7df8c5c4cf0a0cd1
RussianGravy <valentinejones1215@gmail.com>
2024-01-02 15:40:50 -0800

    fixed navbar

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ window.onresize = () => {
     fg.height = fg.width/aspectratio;
 }
 
-const speed = 0.5;
+const speed = 0.6;
 var cur_y = 0
 function updateBackground(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
