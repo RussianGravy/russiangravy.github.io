2bf1c13a9c1b831ba3a1de6effe82aff64338835
RussianGravy <valentinejones1215@gmail.com>
2024-01-04 15:44:36 -0800

    moved bg code + sped up animation

--- elements.js
+++ elements.js
@@ -1,87 +1,60 @@
-//PART ONE
-//important classes and method declarations
+var aspectratio = 2560/1600;
 
-function SiteObject(id){
-    console.log(id + " online");
-    //instance variables
-    this.element = document.getElementById(id);
-    this.lastScreenSize = window.innerWidth;
-    this.width = 0;
-    this.height = 0;
-    this.x = 0;
-    this.y = 0;
-    this.velocity = new Vector2(0, 0);
-    this.acceleration = new Vector2(0, 0);
-    this.done;
-    //methods
-    this.update = function(){
-        //console.log(window.innerWidth + "/" + this.lastScreenSize+ " = " + window.innerWidth/this.lastScreenSize);
-        //updating element
-        this.element.style.left = this.x + "px";
-        this.element.style.top = this.y + "px";
-        //applying velocity and acceleration
-        this.x += this.velocity.getxComp();
-        this.y += this.velocity.getyComp();
-        this.x *= (window.innerWidth/this.lastScreenSize);
-        this.y *= (window.innerWidth/this.lastScreenSize);
-        this.width *= (window.innerWidth/this.lastScreenSize);
-        this.height *= (window.innerWidth/this.lastScreenSize);
-        //stopping when velocity is 0
-        if(this.velocity.getMagnitude() > 0.09){
-            this.velocity.add(this.acceleration);}
-        else{
-            this.velocity.zero();}
-        this.lastScreenSize = window.innerWidth;
-    };
-    this.setPosition = function(x, y){
-        this.x = x;
-        this.y = y;
-        console.log(this.x + ", " + this.y);
-    };
-    this.setVelocity = function(x, y){
-        this.velocity = new Vector2(x, y);
-    };
-    this.setAcceleration = function(){
-        this.acceleration = new Vector2(-1*this.velocity.getxComp()/80, -1*this.velocity.getyComp()/80);
-        // TIME = 40;
-        // xRatio = Math.abs( this.velocity.getxComp() / this.velocity.getMagnitude() );
-        // yRatio = Math.abs( this.velocity.getyComp() / this.velocity.getMagnitude() );
-        // x = 2*(100*xRatio - this.velocity.getxComp())/(TIME*TIME);
-        // y = 2*(100*yRatio - this.velocity.getyComp())/(TIME*TIME);
-        // this.acceleration = new Vector2(x, y);
-    };
-    this.setSize = (w, h) => {
-        this.width = w; 
-        this.height = h;
-        this.element.style.width = this.width + "px";
-        this.element.style.height = this.height + "px";
+const canvas = document.getElementById("background");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+const ctx = canvas.getContext("2d");
+    ctx.width = window.innerWidth;
+    ctx.height = window.innerHeight;
+
+const bg = new Image();
+    bg.src = "images/background.png";
+    bg.width = canvas.width;
+    bg.height = bg.width/aspectratio;
+
+const fg = new Image();
+    fg.src = "images/foreground.png";
+    fg.width = canvas.width;
+    fg.height = fg.width/aspectratio;
+
+var drawBackground = (y) => {
+    for(var i = 0; i  < Math.floor(canvas.height/bg.height)+2; i++){
+        ctx.drawImage(bg, 0, y-bg.height*i, bg.width, bg.height);
     }
 }
 
-function Vector2(x, y){
-    //instance variables
-    this.xComp = x;
-    this.yComp = y;
-    //methods
-    this.getxComp = function(){
-        return this.xComp;
-    };
-    this.getyComp = function(){
-        return this.yComp;
-    };
-    this.getMagnitude = function(){
-        return Math.sqrt(this.xComp*this.xComp + this.yComp*this.yComp);
-    };
-    this.getAngle = function(){};
-    this.add = function(vector){
-        this.xComp += vector.getxComp();
-        this.yComp += vector.getyComp();
-    };
-    this.zero = function(){
-        this.xComp = 0;
-        this.yComp = 0;
-    };
-    toString = function(){
-        return ("(" + this.xComp + ", " + this.yComp + ")");
-    };
+var drawForeground = (y) => {
+    for(var i = 0; i  < Math.floor(canvas.height/fg.height)+2; i++){
+        ctx.drawImage(fg, 0, y+fg.height*i, fg.width, fg.height);
+    }
 }
+
+window.onresize = () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.width = window.innerWidth;
+    ctx.height = window.innerHeight;
+    bg.width = window.innerWidth;
+    bg.height = bg.width/aspectratio;
+    fg.width = window.innerWidth;
+    fg.height = fg.width/aspectratio;
+}
+
+const speed = 1.6;
+var cur_y = 0
+function updateBackground(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBackground(-cur_y+(canvas.height-bg.height));
+    drawForeground(cur_y);
+    cur_y -= speed;
+    if(Math.abs(cur_y) >= fg.height){
+        cur_y = 0;
+    }
+}
+
+// drawImage(image, dx, dy)
+// drawImage(image, dx, dy, dWidth, dHeight)
+// drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+
+// putImageData(imageData, dx, dy)
+// putImageData(imageData, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight)
