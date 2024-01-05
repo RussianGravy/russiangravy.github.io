5021a9f22c6f9114baafc605c097aae515b5d289
RussianGravy <valentinejones1215@gmail.com>
2024-01-04 21:59:05 -0800

    slowed bg + new name font

--- elements.js
+++ elements.js
@@ -40,7 +40,7 @@ window.onresize = () => {
     fg.height = fg.width/aspectratio;
 }
 
-const speed = 1.6;
+const speed = 0.8;
 var cur_y = 0
 function updateBackground(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
